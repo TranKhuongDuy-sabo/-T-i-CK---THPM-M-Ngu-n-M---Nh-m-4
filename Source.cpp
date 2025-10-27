4c0c2a20cc461dc957da53ef7a1f9bea966864eb
Nguyen Xuan Duy <nguyenduyen2572@gmail.com>
2025-10-27 08:36:06 +0700

    update

--- Source.cpp
+++ Source.cpp
@@ -10,11 +10,6 @@ int tich(int a, int b){
 	tich=a*b;
 	return tich;
 }
-int main()
-{
-	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	int result = tich(a,b);
-=======
 int hieu(int a, int b){
 	int hieu=0;
 	hieu=a-b;
@@ -27,8 +22,9 @@ int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	int result=hieu(10,5);
+	int result=tich(10,5);
 	cout<<"Hieu cua a va b la:"<<result;
->>>>>>> 43dc60096d3b21315b59f1f814174f7f471670e8
+	cout<<:tich cua a va b la:"<<result;
 	system("pause");
 	return 0;
 }
