19fa295254cb394a8242c3ac4217a0eac04a592d
Nguyen Xuan Duy <nguyenduyen2572@gmail.com>
2025-10-27 08:31:54 +0700

    update

--- Source.cpp
+++ Source.cpp
@@ -5,10 +5,14 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+int tich(int a, int b){
+	tich=a*b;
+	return tich;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int result = tich(a,b);
 	system("pause");
 	return 0;
 }
