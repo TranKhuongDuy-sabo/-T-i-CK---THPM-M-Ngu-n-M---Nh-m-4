422385a8b2d93cb97b7784b8920bfd93e06c53c3
VanGiap-tech <hoanggiap2k4@gmail.com>
2025-10-27 08:26:22 +0700

    hieu

--- Source.cpp
+++ Source.cpp
@@ -5,10 +5,17 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+int hieu(int a, int b){
+	int hieu=0;
+	hieu=a-b;
+	return hieu;
+}
+
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int result=hieu(10,5);
+	cout<<"Hieu cua a va b la:"<<result;
 	system("pause");
 	return 0;
 }
