e29e51691b9fce0a311b92e7ff54e3ea0230ece0
TranKhuongDuy-sabo <duyt2296@gmail.com>
2025-10-27 09:04:48 +0700

    update

--- Source.cpp
+++ Source.cpp
@@ -16,15 +16,23 @@ int hieu(int a, int b){
 	return hieu;
 }
 
+int tong(int a, int b){
+	int tong = 0;
+	tong = a + b;
+	return tong;
+}
+
 //Themfile de lam ne ae kkk
 
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	int result=hieu(10,5);
-	int result=tich(10,5);
-	cout<<"Hieu cua a va b la:"<<result;
-	cout<<:tich cua a va b la:"<<result;
+	int resultHieu = hieu(10,5);
+	int resultTich = tich(10,5);
+	int resultTong = tong (10,5);
+	cout<<"Hieu cua a va b la:"<<resultHieu;
+	cout<<" Tich cua a va b la : "<<resultTich;
+	cout << "Tong cua a va b la : " << resultTong;
 	system("pause");
 	return 0;
 }
